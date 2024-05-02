503671e22e99d307a556a33aa10e81908e9517f8
mohammadreza2003-af <mohammadreza2003.af@gmail.com>
2024-05-02 12:04:51 +0430

    update

--- backend/api.py
+++ backend/api.py
@@ -181,5 +181,5 @@ if __name__ == "__main__":
         db.create_all()
         # db.drop_all()
         
-    app.run(debug=True,host="0.0.0.0",port=port)
+    app.run(debug=True,host="0.0.0.0",port=5000)
     
